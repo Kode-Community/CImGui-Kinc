d3dc4c572d780ac05e55af436723ce2a013a523f
mundusnine <mundusnine@gmail.com>
2021-01-21 11:52:28 -0500

    Update .gitmodules, c_cpp_properties.json, and 2 more files...

--- kincfile.js
+++ kincfile.js
@@ -3,14 +3,13 @@ let project = new Project('DearImGui');
 project.addDefine('IMGUI_USER_CONFIG="imguiconfig.h"');
 project.addIncludeDir('include');
 
-project.addDefine('CIMGUI_NO_EXPORT');
 project.addIncludeDir('cimgui/imgui');
 project.addIncludeDir('cimgui');
 project.addFiles(
-	'cimgui/imgui/imgui.cpp',
-	'cimgui/imgui/imgui.h',
 	'cimgui/cimgui.h',
 	'cimgui/cimgui.cpp',
+	'cimgui/imgui/imgui.h',
+	'cimgui/imgui/imgui.cpp',
 	'cimgui/imgui/imgui_draw.cpp',
 	'cimgui/imgui/imgui_widgets.cpp',
 	'cimgui/imgui/imgui_demo.cpp'
